411ffa9c5f30cb0efad06fee07d61b15c579156d
mr <1551570564@qq.com>
2020-10-08 21:29:44 +0800

    完成了主页面的功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -2,5 +2,24 @@
 // 而这个过滤器调用的时机是在我们调用 $.ajax() 之后，请求真正发给后台之前调用的： 
 // $.ajax() > ajaxPrefilter过滤器 -> 发送请求给服务器
 $.ajaxPrefilter(function (options) { 
-    options.url = 'http://ajax.frontend.itheima.net'+options.url
+    options.url = 'http://ajax.frontend.itheima.net' + options.url;
+
+    // 统一为有权限的接口，设置 headers 请求头
+    if (options.url.indexOf('/my/') !== -1) { 
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    // 全局统一挂载 complete 回调函数
+    options.complete = function (res) { 
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') { 
+            // 强制清空 token
+            localStorage.removeItem('token')
+            // 强制跳转到登录页面
+            location.href = '/login.html'
+        }
+    }
+
 })
+
