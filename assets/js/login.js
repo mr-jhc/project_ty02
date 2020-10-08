cf005d45219bf444950f27c6682bf1710c13efca
mr <1551570564@qq.com>
2020-10-08 09:35:06 +0800

    完成了登录和注册的功能

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,19 +1,19 @@
-// 入口函数
 $(function () {
-    // 点击注册账号，隐藏登录区域，显示注册区域
-    $("#link_reg").on('click', function () {
-        $('.login-box').hide()
-        $('.reg-box').show()
-    });
-    // 点击去登陆，隐藏注册区域，显示登录区域
-    $("#link_login").on('click', function () {
-        $('.login-box').show()
-        $('.reg-box').hide()
+    // 点击去注册的界面
+    $('#link_reg').on('click', function () {
+        $('.login-box').hide();
+        $('.reg-box').show();
+    })
+    // 点击去登录的界面
+
+    $('#link_login').on('click', function () {
+        $('.login-box').show();
+        $('.reg-box').hide();
     });
 
     // 自定义验证规则
     var form = layui.form;
-    form.verity({
+    form.verify({
         // 密码规则
         pwd: [
             /^[\S]{6,12}$/
@@ -21,63 +21,60 @@ $(function () {
         ],
         // 确认密码规则
         repwd: function (value) {
+            // 选择器必须带空格，选择的是后代中的input，name属性值，为password的那个标签
             var pwd = $(".reg-box input[name=password]").val()
+            // 比较
             if (value !== pwd) {
-                return "两次密码不一样"
+                return "两次密码输入不一致"
             }
         }
     });
-    // 注册功能
-    var layer = layui.layer;
-    $("#form_reg").on("submit", function (e) {
-        // 阻止表单提交
+    // 3.注册功能
+    var layer = layui.layer
+    $('#form_reg').on("submit", function (e) {
+        // 阻止表单默认提交
         e.preventDefault();
-        // 发送ajax
+        // 发送ajax请求
         $.ajax({
-            method: 'POST',
+            method: "POST",
             url: '/api/reguser',
             data: {
-                username: $(".reg-box [name-username]").val(),
-                password: $(".reg-box [name-password]").val()
+                username: $(".reg-box [name=username]").val(),
+                password: $(".reg-box [name=password]").val()
             },
             success: function (res) {
                 // 返回状态判断
                 if (res.status != 0) {
                     return layer.msg(res.message);
                 }
-                // 提交成功处理代码
-                layer.msg(res.message);
-                // 手动切换表单
-                $('#link_login').click()
-                // 重置表单
-                $("#form_reg")[0].reset();
-
-            }
-        })
-    });
-    // 登陆功能
-    $("#form_login").submit(function (e) {
-        // 阻止表单提交
-        e.preventDefault();
-        // 发送ajax
-        $.ajax({
-            method: 'POST',
-            url: '/api/login',
-            data: $(this).serialize(),
-            success: function (res) {
-                // 校验返回状态
-                if (res.status !== 0) {
-                    return layer.msg(res.message)
-                }
-                // 提示信息，保存token，跳转界面
-                layer.msg("恭喜您，登陆成功")
-                // 保存token，未来的接口使用token
-                localStorage.setItem("token", res.token);
-                // 跳转
-                location.href = "/index.html"
-
+                // 提交成功后处理代码
+                layer.msg(res.message)
+                // 提示完成以后自己手动点击
+                $("#link_login").click()
             }
         })
     })
-
+    $('#form_login').on("submit", function (e) {
+        // 阻止表单默认提交
+        e.preventDefault();
+        // 发送ajax请求
+        $.ajax({
+            method: "POST",
+            url: '/api/login',
+            // 获取表单中的数据
+            data: $(this).serialize(),
+            success: function (res) {
+                // 返回状态判断
+                if (res.status != 0) {
+                    return layer.msg(res.message);
+                }
+                // 提交成功后处理代码
+                layer.msg("登陆成功")
+                //    存储token字符串保存到localStorage中
+                localStorage.setItem('token', res.token)
+                // 页面跳转
+                location.href = "/index.html"
+            }
+        })
+    })
 })
